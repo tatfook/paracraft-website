35dfbf7192891dc41c6083dc33627f26a587486d
DDXY1230 <2502752288@qq.com>
2019-03-08 10:59:40 +0800

    remove useless file add components

--- app/controller/edition.js
+++
@@ -1,8 +0,0 @@
-module.exports = app => {
-  return class EditionController extends app.Controller {
-    async homepage() {
-      const { ctx } = this
-      await ctx.renderClient('homepage/index.js')
-    }
-  }
-}
---
+++ app/web/component/common/CommonHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="common-header">这是头部</div>
+</template>
+<script>
+export default {
+  name: 'CommonHeader'
+}
+</script>
+<style lang="scss">
+.common-header{
+  border: 1px solid red;
+}
+</style>
+
+
