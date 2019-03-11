1417cbf7bc48ebb3c0dee7e3f36ad3e4262b71fa
DDXY1230 <2502752288@qq.com>
2019-03-11 15:00:54 +0800

    add commonheader and commonfooter

--- app/web/component/common/CommonFooter.vue
+++ app/web/component/common/CommonFooter.vue
@@ -1,6 +1,61 @@
 <template>
   <div class="common-footer">
-    这是底部
+    <div class="common-footer-content">
+      <div class="common-footer-content-part">
+        <ul class="common-footer-content-part-list">
+          <li class="common-footer-content-part-list-title">产品</li>
+          <li>Paracraft 创意空间</li>
+          <li><a href="/download">免费下载</a></li>
+          <li>作品社区</li>
+          <li>NPL动画与编程大赛</li>
+        </ul>
+      </div>
+      <div class="common-footer-content-part">
+        <ul class="common-footer-content-part-list">
+          <li class="common-footer-content-part-list-title">服务</li>
+          <li><a href="/download">我们的优势</a></li>
+          <li>服务项目</li>
+          <li>价格</li>
+        </ul>
+      </div>
+      <div class="common-footer-content-part">
+        <ul class="common-footer-content-part-list">
+          <li class="common-footer-content-part-list-title">致社区</li>
+          <li>致教育者</li>
+          <li><a href="/download">给父母的话</a></li>
+          <li>致开发者</li>
+          <li>对爱好编程的你说</li>
+        </ul>
+      </div>
+      <div class="common-footer-content-part">
+        <ul class="common-footer-content-part-list">
+          <li class="common-footer-content-part-list-title">关于我们</li>
+          <li>公司简介</li>
+          <li><a href="/download">新闻动态</a></li>
+        </ul>
+      </div>
+      <div class="common-footer-content-part">
+        <ul class="common-footer-content-part-list">
+          <li class="common-footer-content-part-list-title">联系我们</li>
+          <li>电话：0755-86967012</li>
+          <li>邮箱：service@KeepWork.com.cn</li>
+        </ul>
+      </div>
+      <div class="common-footer-content-part common-footer-content-part-last">
+        <ul class="common-footer-content-part-list">
+          <li class="common-footer-content-part-list-title">关于Paracraft</li>
+          <li>
+            <img class="common-footer-content-part-list-weixin" src="@/asset/images/weixin.png" alt="">
+          </li>
+          <li>Paracraft公众号</li>
+        </ul>
+      </div>
+    </div>
+    <div class="common-footer-bottom">
+      Paracraft
+      |
+      <span class="common-footer-bottom-text">粤ICP备14002196号-2© Tatfook</span>
+    </div>
   </div>
 </template>
 <script>
@@ -9,9 +64,49 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-.common-footer{
-  // height: 100px;
-  border: 1px solid black
+.common-footer {
+  &-content {
+    max-width: 1200px;
+    margin: 0 auto;
+    display: flex;
+    &-part {
+      flex: 1;
+      color: #fff;
+      &-list {
+        list-style: none;
+        padding: 0 0 20px;
+        font-size: 14px;
+        line-height: 35px;
+        a{
+          text-decoration: none;
+          color: #b4b4b4;
+        }
+        &-title {
+          color: #fff;
+          padding: 30px 0 8px;
+        }
+        li + li {
+          color: #b4b4b4;
+        }
+        &-weixin{
+          width: 122px;
+        }
+      }
+      &-last{
+        text-align: center;
+      }
+    }
+  }
+  &-bottom {
+    font-size: 13px;
+    color: #fff;
+    text-align: center;
+    line-height: 65px;
+    background: #181818;
+    &-text {
+      color: #666;
+    }
+  }
 }
 </style>
 
--- app/web/component/common/CommonHeader.vue
+++ app/web/component/common/CommonHeader.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="common-header">这是头部</div>
+  <div class="common-header">
+    <el-menu mode="horizontal">
+      <el-menu-item><img src="@/asset/images/paracraft.png" alt="paracraft">Paracraft 创意空间</el-menu-item>
+      <el-menu-item>我们的优势</el-menu-item>
+      <el-menu-item>服务项目</el-menu-item>
+      <el-menu-item>价格</el-menu-item>
+      <el-menu-item class="pull-right">
+        <el-button type="primary">免费体验</el-button>
+      </el-menu-item>
+    </el-menu>
+  </div>
 </template>
 <script>
 export default {
@@ -7,8 +17,26 @@ export default {
 }
 </script>
 <style lang="scss">
-.common-header{
-  border: 1px solid red;
+.common-header {
+  max-width: 1200px;
+  margin: 0 auto;
+  .el-menu--horizontal.el-menu {
+    background: rgb(5, 16, 41);
+    min-height: 82px;
+    border: none;
+    .el-menu-item {
+      height: 82px;
+      line-height: 82px;
+      color: #fff;
+      font-size: 14px;
+      &:hover {
+        background: rgb(5, 16, 41);
+      }
+    }
+  }
+  .pull-right {
+    float: right !important;
+  }
 }
 </style>
 
