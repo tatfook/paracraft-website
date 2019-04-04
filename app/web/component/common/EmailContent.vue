10f3688ee9c392a367d04bc89d864d795cb114f3
DDXY1230 <2502752288@qq.com>
2019-04-04 17:40:34 +0800

    fix style

--- app/web/component/common/EmailContent.vue
+++ app/web/component/common/EmailContent.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="email-border">
     <div class="email-border-content">
-      <p class="email-border-content-main">您好！</p>
+      <p class="email-border-content-main">{{name}}，您好！</p>
       <p class="email-border-content-main">
         我是Paracraft的作者李西峙。谢谢您对Paracraft的兴趣！
+      </p>
+      <p class="email-border-content-main">
         我们已经收到了您填写的信息，我们的工作人员会尽快跟您联系。
-        很高兴您选择了Paracraft。
       </p>
       <p class="email-border-content-main">
         1989年，7岁的我照着一本我父亲给我的书编写了我人生中第一个程序，并从此喜欢上了编程，小学期间我完成了大量个人作品，到了随心所欲的状态。通过Paracraft，我希望带给孩子们同我相似的学习体验。
@@ -13,18 +14,21 @@
       <p class="email-border-content-main">
         期间您有任何技术问题也可以直接Email我 。
       </p>
-      <p class="email-border-content-main">
+      <!-- <p class="email-border-content-main">
         祝好！
-      </p>
+      </p> -->
+      <br>
+      <br>
+      <br>
       <p class="email-border-content-ending">李西峙</p>
       <p class="email-border-content-ending">CEO & NPL语言与Paracraft创始人</p>
       <p class="email-border-content-ending">Email：lixizhi@paraengine.com</p>
       <div class="email-border-content-box">
         <p>通过下面链接了解Paracraft：</p>
-        <p><a class="email-border-content-box-link" href="http://paracraft.keepwork.com/download?lang=zh" target="_blank">下载Paracraft </a></p>
-        <p><a class="email-border-content-box-link" href="http://cdn.keepwork.com/paracraft/book/paracraft_book_sample.pdf" target="_blank">《Paracraft编程入门》</a></p>
-        <p>关注微信号Paracraft ，观看部分教学视频</p>
-        <p><img src="@/asset/images/weixin.png" class="email-border-content-box-email" alt=""></p>
+        <p><a class="email-border-content-box-link" href="http://paracraft.keepwork.com/download?lang=zh" target="_blank">下载Paracraft</a>：推荐Windows版本</p>
+        <p><a class="email-border-content-box-link" href="http://cdn.keepwork.com/paracraft/book/paracraft_book_sample.pdf" target="_blank">《Paracraft编程入门》</a>：预览版PDF文档</p>
+        <!-- <p>关注微信号Paracraft ，观看部分教学视频</p> -->
+        <!-- <p><img src="@/asset/images/weixin.png" class="email-border-content-box-email" alt=""></p> -->
       </div>
     </div>
   </div>
@@ -44,7 +48,7 @@ export default {
 </script>
 <style lang="scss">
 .email-border {
-  max-width: 624px;
+  max-width: 627px;
   margin: 50px auto;
   background: repeating-linear-gradient(
     -45deg,
@@ -60,9 +64,10 @@ export default {
     p {
       color: #333;
       font-size: 14px;
+      line-height: 26px;
     }
     &-main {
-      margin: 40px;
+      margin: 26px;
     }
     &-ending {
       margin: 8px 40px;
